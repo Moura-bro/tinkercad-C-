d24c858c55384b7c8fde5f0796aeaaf7920321a6
Rafael Moura <mbarrientorafael@gmail.com>
2025-05-16 15:52:23 -0300

    TABUADA

---
+++ tabuada_intl_261.ino
@@ -0,0 +1,38 @@
+int Numero = 0;
+int Resultado = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println("Tabuada inteligente");
+  Serial.println("Seja Bem-vindo");
+  Serial.println("Qual tabuada voce quer, que eu exiba?");
+  while(! Serial.available ()){}
+  Numero = Serial.parseInt ();
+  //-------------------^"Menu"--->Cabeça da maquina|  
+  for(int contador = 1; contador <=10;contador++){ 
+    
+  Resultado  = Numero * contador ;
+
+    
+Serial.print("("+String(Numero));    
+Serial.print(" * ");    
+Serial.print(Resultado);
+Serial.println(")");
+  }
+
+
+}
+
+void loop()
+{
+
+  
+  
+  
+  
+  
+  
+  
+}
